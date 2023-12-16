a19b3962b6abe98d1747475df209bad90b384c92
ochelle <alexizochelle@gmail.com>
2023-12-16 12:59:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer (){
+    bestCustomer = ('not bob');
+}
+function overwriteBestCustomer(){
+    bestCustomer=('maybe bob');
+}
+const leastFavoriteCustomer = 'ochelle';
+function changeLeastFavoriteCustomer(){
+    console.log(leastFavoriteCustomer = "xyz");
+}
